a87f9c2f621ac9fdc3472cdedbcbf211e8a1e578
Kyle <johnkyleobusan@gmail.com>
2024-04-15 21:43:46 +0800

    feat:taposna

--- frontend/src/components/NavPage.jsx
+++ frontend/src/components/NavPage.jsx
@@ -6,8 +6,8 @@ function NavPage() {
        
 <nav className="bg-blue border-gray-200 dark:bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+  <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+      <img src="https://as1.ftcdn.net/v2/jpg/03/14/89/44/1000_F_314894431_kjM17JWNnBXWmw6fw5FWgBMrgOyVjiPJ.jpg" className="h-8" alt="Flowbite Logo" />
       <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Techbite</span>
   </a>
   <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
